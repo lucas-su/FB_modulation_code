5030d3414a427b34deb6b3ac9c767e0e575e6a55
luc <lucas.schootuiterkamp@gmail.com>
2022-02-17 16:11:00 +0100

    bounce_eval_model

--- bounce_eval_model.m
+++ bounce_eval_model.m
@@ -98,8 +98,8 @@ for part=1:19
                     end
                     if i ~= length(contact_shift) %prevent out of bound index
                         if contact_shift(i+1) == 0
-                            mean_last_bounce = mean(force_model(start_contact:i,1));
-                            temp_array = arrayfun(@(m) {abs(mean_last_bounce-m)/1000}, force_model(start_contact:i));
+                            mean_last_bounce = mean(force_sensor(start_contact:i,1));
+                            temp_array = arrayfun(@(m) {abs(mean_last_bounce-m)/1000}, force_sensor(start_contact:i));
                             if bounce_cond == 0
                                 diff.raw(part, ncontacts.raw)=sum(vertcat(temp_array{:}));
                             elseif bounce_cond == 2
