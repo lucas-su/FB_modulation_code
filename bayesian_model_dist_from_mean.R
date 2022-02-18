35673f2c22fd5ad28c3fc757b969f273bd2a648d
luc <lucas.schootuiterkamp@gmail.com>
2022-02-18 10:37:05 +0100

    change bayesian models to brm, fam to exponential

--- bayesian_model_dist_from_mean.R
+++ bayesian_model_dist_from_mean.R
@@ -8,24 +8,26 @@ library(devtools)
 library(knitr)
 library(mascutils)
 library(bayr)
-
+library(brms)
 ##
 
 dist_from_mean <- read_csv("C:\\Users\\admin\\pacof\\data\\FB_modulation_code\\dist_from_mean_model.csv", 
                            col_names = c('part','dist','cond'))
-labels_cond=c("raw", "average", "low pass", "scale")
-labels_part <- as.character(c(3:21))
+#labels_cond=c("raw", "average", "low pass", "scale")
+#labels_part <- as.character(c(3:21))
 
 ## 
-lm_P_C <-  stan_lmer(dist ~ 1 + (1|part) + (1|cond),
-                      data = dist_from_mean )
+lm_P_C <-  brm(dist ~ 1 + (1|part) + (1|cond),
+               data = dist_from_mean,
+               family = exponential(),
+               control = list(adapt_delta = 0.99))
 
 coefs_P_C <- coef(posterior(lm_P_C), interval=0.9)
 
 coefs_P_C
 
-coefs_norm_P_C_part <- slice(dplyr::select(coefs_P_C, center, lower, upper), 2:20)+  1.84 #(dplyr::select(coefs,center),1)
-coefs_norm_P_C_cond <- slice(dplyr::select(coefs_P_C, center, lower, upper), 21:24)+  1.84 #(dplyr::select(coefs,center),1)
+coefs_norm_P_C_part <- slice(dplyr::select(coefs_P_C, center, lower, upper), 6:25)+  0.48 #(dplyr::select(coefs,center),1)
+coefs_norm_P_C_cond <- slice(dplyr::select(coefs_P_C, center, lower, upper), 2:5)+  0.48 #(dplyr::select(coefs,center),1)
 
 coefs_norm_P_C_cond$labels_ = c("raw", "average", "low pass", "scale")
 coefs_norm_P_C_part$labels_ <- as.character(c(3:21))
@@ -36,7 +38,7 @@ ggplot(coefs_norm_P_C_part, aes(labels_, center)) +        # ggplot2 plot with 9
   geom_point() +
   geom_errorbar(aes(ymin = lower, ymax = upper))+
   scale_x_discrete("Participant", limits=coefs_norm_P_C_part$labels_)+
-  scale_y_continuous("Deviation from mean", breaks=c(1:6))
+  scale_y_continuous("Deviation from mean")
 ggsave("C:\\Users\\admin\\pacof\\data\\FB_modulation_code\\dev_from_mean_model_P_C_part.pdf")
 
 ggplot(coefs_norm_P_C_cond, aes(labels_, center)) +        # ggplot2 plot with 90% credibility limits
@@ -48,8 +50,10 @@ ggsave("C:\\Users\\admin\\pacof\\data\\FB_modulation_code\\dev_from_mean_model_P
 
 ##
 
-lm_C <-  stan_lmer(dist ~ 1+ (1|cond),
-                      data = dist_from_mean )
+lm_C <-  brm(dist ~ 1+ (1|cond),
+             data = dist_from_mean,
+             family = exponential(),
+             control = list(adapt_delta = 0.99))
 
 coefs_C <- coef(posterior(lm_C), interval=0.9)
 
@@ -74,7 +78,9 @@ dist_from_mean_outliers_removed <- subset(x= dist_from_mean,
                                             part != 14)
 
 m_P_C_No_out <-  stan_lmer(dist ~ 1 + (1|part) + (1|cond),
-                    data = dist_from_mean_outliers_removed  )
+                    data = dist_from_mean_outliers_removed,
+                    family = exponential(),
+                    control = list(adapt_delta = 0.99))
 
 coefs_P_C_No_out <- coef(posterior(m_P_C_No_out), interval=0.9)
 
